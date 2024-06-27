95f2a54d8d6b49bf83901754337bd40057085bbd
SerDigital64 <serdigital64@gmail.com>
2024-06-27 05:18:17 +0000

    fix: reorder conditional var declaration

--- src/lib/gcloud.bash
+++ src/lib/gcloud.bash
@@ -2,12 +2,13 @@ function sysden64_gcloud_setup() {
   bl64_dbg_app_show_function "$@"
   local home="$1"
   local target="${home}/.config/gcloud"
-  local vault="${DEV_PATH_PROF_VAULT}/gcloud"
+  local vault=''
   local model="${SYSDEN64_PATH_ETC}/gcloud"
 
   bl64_msg_show_phase 'prepare GCloud CLI'
   bl64_msg_show_task "setup GCloud CLI (${target})"
   if bl64_lib_flag_is_enabled "$SYSDEN64_USE_DEVBIN64"; then
+    vault="${DEV_PATH_PROF_VAULT}/gcloud"
     bl64_fs_create_dir "$BL64_VAR_DEFAULT" "$BL64_VAR_DEFAULT" "$BL64_VAR_DEFAULT" \
       "$vault" \
       "${vault}/configurations" &&
--- src/lib/gpg.bash
+++ src/lib/gpg.bash
@@ -2,11 +2,12 @@ function sysden64_gpg_setup() {
   bl64_dbg_app_show_function "$@"
   local home="$1"
   local target="${home}/.gnupg"
-  local vault="${DEV_PATH_PROF_VAULT}/gnupg"
+  local vault=''
 
   bl64_msg_show_phase 'prepare GNUPG'
   bl64_msg_show_task "setup GNUPG (${target})"
   if bl64_lib_flag_is_enabled "$SYSDEN64_USE_DEVBIN64"; then
+    vault="${DEV_PATH_PROF_VAULT}/gnupg"
     bl64_fs_create_dir "$BL64_VAR_DEFAULT" "$BL64_VAR_DEFAULT" "$BL64_VAR_DEFAULT" \
       "$vault" &&
       bl64_fs_create_symlink \
