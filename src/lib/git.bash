0a1e9f460883af43543c30b05d948ea57ddb9d50
SerDigital64 <serdigital64@gmail.com>
2024-07-14 14:35:02 +0000

    feat: do not replace config

--- src/lib/git.bash
+++ src/lib/git.bash
@@ -2,6 +2,7 @@ function sysden64_git_setup() {
   bl64_dbg_app_show_function "$@"
   local home="$1"
   local model="${SYSDEN64_PATH_ETC}/git"
+  local target="${home}"
 
   bl64_msg_show_phase 'prepare GIT'
   bl64_fs_path_copy \
@@ -9,13 +10,19 @@ function sysden64_git_setup() {
     "$BL64_VAR_DEFAULT" \
     "$BL64_VAR_DEFAULT" \
     "$BL64_VAR_DEFAULT" \
-    "$home" \
-    "${model}/.gitconfig" &&
-    bl64_fs_path_copy \
-      "$BL64_VAR_DEFAULT" \
-      "$BL64_VAR_DEFAULT" \
-      "$BL64_VAR_DEFAULT" \
-      "$BL64_VAR_DEFAULT" \
-      "${home}/${SYSDEN64_PATH_SHELLENV}" \
-      "${model}/${SYSDEN64_PATH_SHELLENV}"/*.env
+    "${home}/${SYSDEN64_PATH_SHELLENV}" \
+    "${model}/${SYSDEN64_PATH_SHELLENV}"/*.env ||
+    return $?
+
+  [[ -f "${target}/.gitconfig" ]] &&
+    bl64_msg_show_warning 'already configured. No further action taken' &&
+    return 0
+
+  bl64_fs_path_copy \
+    "$BL64_VAR_DEFAULT" \
+    "$BL64_VAR_DEFAULT" \
+    "$BL64_VAR_DEFAULT" \
+    "$BL64_VAR_DEFAULT" \
+    "$target" \
+    "${model}/.gitconfig"
 }
