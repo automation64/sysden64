3a2abf0dd9b08f68b54705131cc598df823b6b98
serdigital64 <serdigital64@gmail.com>
2024-08-18 11:35:27 +0000

    doc: update

---
+++ docs/snippets/module-ephemeral.bash
@@ -0,0 +1,14 @@
+function sysden64_X_MODULE_ID_X_setup() {
+  bl64_dbg_app_show_function "$@"
+  local home="$1"
+  local model="${SYSDEN64_PATH_ETC}/X_MODULE_ID_X"
+
+  bl64_msg_show_phase 'prepare X_MODULE_ID_X'
+  bl64_fs_path_copy \
+    "$BL64_VAR_DEFAULT" \
+    "$BL64_VAR_DEFAULT" \
+    "$BL64_VAR_DEFAULT" \
+    "$BL64_VAR_DEFAULT" \
+    "${home}/${SYSDEN64_PATH_SHELLENV}" \
+    # example # "${model}/${SYSDEN64_PATH_SHELLENV}"/*.env
+}
