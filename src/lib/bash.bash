0367de7d4f20017c483fda5f3cbe2d3733339fd5
serdigital64 <serdigital64@gmail.com>
2024-10-06 07:21:05 +0000

    refact: migrate to bl64

--- src/lib/bash.bash
+++ src/lib/bash.bash
@@ -1,19 +1,20 @@
 function sysden64_bash_setup() {
   bl64_dbg_app_show_function "$@"
   local home="$1"
-  local model="${SYSDEN64_PATH_ETC}/bash"
+  local path_bash_profile="${home}/.bash_profile"
+  local path_bash_rc="${home}/.bashrc"
 
   bl64_msg_show_phase 'prepare Bash'
   ! bl64_bsh_command_is_executable 'bash' &&
-    bl64_msg_show_warning 'tool not detected. No further action taken' && return 0
+    bl64_msg_show_warning "$SYSDEN64_TXT_NOT_DETECTED" && return 0
 
   bl64_msg_show_task "Setup user's bash profile (${home})"
-  bl64_fs_path_copy \
-    '0700' \
-    "$BL64_VAR_DEFAULT" \
-    "$BL64_VAR_DEFAULT" \
-    "$BL64_VAR_DEFAULT" \
-    "$home" \
-    "${model}/.bashrc" \
-    "${model}/.bash_profile"
+  echo "$SYSDEN64_TXT_WATERMARK" >"$path_bash_profile" &&
+    echo "$SYSDEN64_TXT_WATERMARK" >"$path_bash_rc" &&
+    bl64_bsh_profile_bash_generate >>"$path_bash_profile" &&
+    bl64_bsh_profile_rc_generate >>"$path_bash_rc" &&
+    bl64_fs_path_permission_set \
+      '0700' "$BL64_VAR_DEFAULT" "$BL64_VAR_DEFAULT" "$BL64_VAR_DEFAULT" "$BL64_VAR_DEFAULT" \
+      "$path_bash_profile" \
+      "$path_bash_rc"
 }
