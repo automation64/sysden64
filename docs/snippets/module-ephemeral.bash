4bbacfc08c61693beb1afc34d48953e417a406e4
X_GIT_USER_NAME_X <X_GIT_USER_EMAIL_X>
2025-03-01 09:34:00 +0000

    refact: normalize names

--- docs/snippets/module-ephemeral.bash
+++ docs/snippets/module-ephemeral.bash
@@ -1,7 +1,7 @@
 function sysden64_X_MODULE_ID_X_setup() {
   bl64_dbg_app_show_function "$@"
   local home="$1"
-  local model="${SYSDEN64_PATH_ETC}/X_MODULE_ID_X"
+  local model="${SYSDEN64_PATH_ETC_SYSTEM}/X_MODULE_ID_X"
 
   bl64_msg_show_phase 'prepare X_MODULE_ID_X'
   bl64_fs_path_copy \
