f579ab8cbd133f95b95adac8f42d09906a8faa48
X_GIT_USER_NAME_X <X_GIT_USER_EMAIL_X>
2025-03-03 14:01:09 +0000

    feat: add homebrew

---
+++ src/lib/homebrew.bash
@@ -0,0 +1,28 @@
+function module_homebrew_setup() {
+  bl64_dbg_app_show_function "$@"
+  local home="$1"
+  local module_type="$SYSDEN64_MODULE_TYPE_SHARED"
+  local model='homebrew'
+  local source=''
+  local location_darwin='/opt/homebrew'
+  local location_linux='/home/linuxbrew/.linuxbrew'
+
+  module_profile_switch_allow "$module_type" && return 0
+
+  [[ ! -d "$location_darwin" && ! -d "$location_linux" ]] &&
+    bl64_dbg_app_show_info "$SYSDEN64_TXT_NOT_DETECTED" && return 0
+  bl64_msg_show_phase 'prepare HomeBrew'
+
+  module_create_shared "$module_type" "$model" &&
+  source="$(module_set_model "$module_type" "$model")" ||
+  return $?
+
+  bl64_msg_show_task "setup environment variables (${home}/${SYSDEN64_PATH_SHELLENV})"
+  bl64_fs_path_copy \
+    "$BL64_VAR_DEFAULT" \
+    "$BL64_VAR_DEFAULT" \
+    "$BL64_VAR_DEFAULT" \
+    "$BL64_VAR_DEFAULT" \
+    "${home}/${SYSDEN64_PATH_SHELLENV}" \
+    "${source}/${SYSDEN64_PATH_SHELLENV}-${BL64_OS_TYPE}"/*.env
+}
