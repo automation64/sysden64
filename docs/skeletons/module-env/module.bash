0489b19f25a13939704ab4ff03c3b5e34f5019c1
SerDigital64 <serdigital64@gmail.com>
2025-03-12 21:22:31 +0100

    doc: update

---
+++ docs/skeletons/module-env/module.bash
@@ -0,0 +1,28 @@
+function module_X_MODULE_ID_X_setup() {
+   bl64_dbg_app_show_function "$@"
+  local home="$1"
+  local module_type="$SYSDEN64_MODULE_TYPE_SHARED"
+  local model='X_MODULE_ID_X'
+  local config='X_CONFIG_X'
+  local target="${home}/${config}"
+  local source=''
+
+  module_profile_switch_allow "$module_type" && return 0
+
+  ! bl64_bsh_command_is_executable 'X_COMMAND_X' &&
+    bl64_dbg_app_show_info "$SYSDEN64_TXT_NOT_DETECTED" && return 0
+  bl64_msg_show_phase 'prepare X_APP_X'
+
+  module_create_shared "$module_type" "$model" &&
+  source="$(module_set_model "$module_type" "$model")" ||
+  return $?
+
+  bl64_msg_show_task "setup environment variables (${home}/${SYSDEN64_PATH_SHELLENV})"
+  bl64_fs_path_copy \
+    "$BL64_VAR_DEFAULT" \
+    "$BL64_VAR_DEFAULT" \
+    "$BL64_VAR_DEFAULT" \
+    "$BL64_VAR_DEFAULT" \
+    "${home}/${SYSDEN64_PATH_SHELLENV}" \
+    "${source}/${SYSDEN64_PATH_SHELLENV}"/*.env
+}
