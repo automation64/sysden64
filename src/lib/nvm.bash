94fbad42ed5b5c2381aa882826a04acbe2f8e178
X_GIT_USER_NAME_X <X_GIT_USER_EMAIL_X>
2025-03-14 19:03:20 +0000

    feat: add nvm

---
+++ src/lib/nvm.bash
@@ -0,0 +1,28 @@
+function module_nvm_setup() {
+  bl64_dbg_app_show_function "$@"
+  local home="$1"
+  local module_type="$SYSDEN64_MODULE_TYPE_SHARED"
+  local model='nvm'
+  local config='nvm'
+  local target="${home}/${config}"
+  local source=''
+
+  module_profile_switch_allow "$module_type" && return 0
+
+  [[ -s "${HOME}/.nvm/nvm.sh" ]] &&
+    bl64_dbg_app_show_info "$SYSDEN64_TXT_NOT_DETECTED" && return 0
+  bl64_msg_show_phase 'prepare NVM'
+
+  module_create_shared "$module_type" "$model" &&
+    source="$(module_set_model "$module_type" "$model")" ||
+    return $?
+
+  bl64_msg_show_task "setup environment variables (${home}/${SYSDEN64_PATH_SHELLENV})"
+  bl64_fs_path_copy \
+    "$BL64_VAR_DEFAULT" \
+    "$BL64_VAR_DEFAULT" \
+    "$BL64_VAR_DEFAULT" \
+    "$BL64_VAR_DEFAULT" \
+    "${home}/${SYSDEN64_PATH_SHELLENV}" \
+    "${source}/${SYSDEN64_PATH_SHELLENV}"/*.env
+}
