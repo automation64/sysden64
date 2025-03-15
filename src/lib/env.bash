46b9aaf334c34452aec0ff348b1eccfbbaea2d1c
X_GIT_USER_NAME_X <X_GIT_USER_EMAIL_X>
2025-03-15 18:32:23 +0000

    feat: add bkp

--- src/lib/env.bash
+++ src/lib/env.bash
@@ -1,15 +1,21 @@
+# Version: 1.1.0
 function module_env_setup() {
   bl64_dbg_app_show_function "$@"
   local home="$1"
   local module_type="$SYSDEN64_MODULE_TYPE_SHARED"
   local model='env'
   local source=''
-  bl64_msg_show_phase 'prepare shell-env'
+  local config='.env.d'
+  local target="${home}/${config}"
+
+  bl64_msg_show_phase 'prepare shell env repository'
 
   source="$(module_set_model "$module_type" "$model")" ||
-  return $?
+    return $?
 
-  bl64_msg_show_task "Populate env store  (${SYSDEN64_PATH_SHELLENV})"
+  module_config_backup "$model" "$target" || return $?
+
+  bl64_msg_show_task "Populate env store (${SYSDEN64_PATH_SHELLENV})"
   # shellcheck disable=SC2086
   bl64_fs_path_copy \
     "$BL64_VAR_DEFAULT" \
