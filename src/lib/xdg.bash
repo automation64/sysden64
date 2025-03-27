89acb96b91feb4c18b3f6682791874501d158056
X_GIT_USER_NAME_X <X_GIT_USER_EMAIL_X>
2025-03-27 12:16:59 +0000

    real: promote 3.5.1

--- src/lib/xdg.bash
+++ src/lib/xdg.bash
@@ -2,8 +2,24 @@
 function module_xdg_setup() {
   bl64_dbg_app_show_function "$@"
   local home="$1"
+  local module_type="$SYSDEN64_MODULE_TYPE_SHARED"
+  local model='xdg'
+  local source=''
 
   bl64_msg_show_task 'prepare XDG'
   bl64_bsh_xdg_create \
-    "$home"
+    "$home" ||
+    return $?
+
+  source="$(module_set_model "$module_type" "$model")" ||
+    return $?
+
+  bl64_msg_show_task "setup environment variables (${home}/${SYSDEN64_PATH_SHELLENV})"
+  bl64_fs_path_copy \
+    "$BL64_VAR_DEFAULT" \
+    "$BL64_VAR_DEFAULT" \
+    "$BL64_VAR_DEFAULT" \
+    "$BL64_VAR_DEFAULT" \
+    "${home}/${SYSDEN64_PATH_SHELLENV}" \
+    "${source}/${SYSDEN64_PATH_SHELLENV}"/*.env
 }
