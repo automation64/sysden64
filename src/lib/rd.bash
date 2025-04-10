4e39e8ecfe1a8a27247a78f5f34684d9f014b382
Maximiliano (Max) Morales <165780911+atinary-mmorales@users.noreply.github.com>
2025-04-10 07:50:03 +0200

    feat: add rd, zellij, lsd configs (#55)

--- src/lib/rd.bash
+++ src/lib/rd.bash
@@ -1,3 +1,4 @@
+# Version: 1.1.0
 function module_rd_setup() {
   bl64_dbg_app_show_function "$@"
   local home="$1"
@@ -5,15 +6,48 @@ function module_rd_setup() {
   local model='rd'
   local source=''
   local config='rd'
-  local target="${home}/${config}"
+  local target="${home}"
 
-  [[ ! -x "$HOME/.rd/bin/docker" ]] &&
+  [[ "$BL64_OS_TYPE" != "$BL64_OS_TYPE_MACOS" ]] &&
+    [[ ! -x '/Applications/Rancher Desktop.app/Contents/Resources/resources/darwin/bin/docker' ]] &&
     bl64_dbg_app_show_info "$SYSDEN64_TXT_NOT_DETECTED" && return 0
   bl64_msg_show_phase 'prepare Rancher Desktop'
 
-  source="$(module_set_model "$module_type" "$model")" ||
-  return $?
+  source="$(module_set_model "$module_type" "$model")" &&
+    module_rd_setup_env "$home" "$source" &&
+    module_rd_setup_config "$home" "$source" "$config" "$model" "$target"
+}
 
+function module_rd_setup_config() {
+  bl64_dbg_app_show_function "$@"
+  local home="$1"
+  local source="$2"
+  local config="$3"
+  local model="$4"
+  local target="$5"
+  local config_file=''
+
+  target="${target}/Library/Preferences"
+  config_file='io.rancherdesktop.profile.defaults.plist'
+
+  module_config_backup "$model" "${target}/${config_file}" ||
+    return $?
+
+  bl64_msg_show_task "promote configuration from model (${model}/${config})"
+  # shellcheck disable=SC2086
+  bl64_fs_path_copy \
+    "$BL64_VAR_DEFAULT" \
+    "$BL64_VAR_DEFAULT" \
+    "$BL64_VAR_DEFAULT" \
+    "$BL64_VAR_DEFAULT" \
+    "$target" \
+    "${source}/${config}/profile/${BL64_OS_TYPE}/${config_file}"
+}
+
+function module_rd_setup_env() {
+  bl64_dbg_app_show_function "$@"
+  local home="$1"
+  local source="$2"
   bl64_msg_show_task "setup environment variables (${home}/${SYSDEN64_PATH_SHELLENV})"
   bl64_fs_path_copy \
     "$BL64_VAR_DEFAULT" \
--- src/lib/zellij.bash
+++ src/lib/zellij.bash
@@ -1,18 +1,48 @@
-# Version: 1.0.0
+# Version: 1.1.0
 function module_zellij_setup() {
   bl64_dbg_app_show_function "$@"
   local home="$1"
   local module_type="$SYSDEN64_MODULE_TYPE_SHARED"
   local model='zellij'
   local source=''
+  local config='.config/zellij'
+  local target="${home}/${config}"
 
   [[ -z "$(bl64_bsh_command_locate 'zellij')" ]] &&
     bl64_dbg_app_show_info "$SYSDEN64_TXT_NOT_DETECTED" && return 0
   bl64_msg_show_phase 'prepare Zellij'
 
-  source="$(module_set_model "$module_type" "$model")" ||
-  return $?
+  source="$(module_set_model "$module_type" "$model")" &&
+    module_zellij_setup_env "$home" "$source" &&
+    module_zellij_setup_config "$home" "$source" "$config" "$model" "$target"
+}
 
+function module_zellij_setup_config() {
+  bl64_dbg_app_show_function "$@"
+  local home="$1"
+  local source="$2"
+  local config="$3"
+  local model="$4"
+  local target="$5"
+
+  module_config_backup "$model" "$target" ||
+    return $?
+
+  bl64_msg_show_task "promote configuration from model (${model}/${config})"
+  # shellcheck disable=SC2086
+  bl64_fs_path_copy \
+    "$BL64_VAR_DEFAULT" \
+    "$BL64_VAR_DEFAULT" \
+    "$BL64_VAR_DEFAULT" \
+    "$BL64_VAR_DEFAULT" \
+    "${home}/.config" \
+    "${source}/${config}"
+}
+
+function module_zellij_setup_env() {
+  bl64_dbg_app_show_function "$@"
+  local home="$1"
+  local source="$2"
   bl64_msg_show_task "setup environment variables (${home}/${SYSDEN64_PATH_SHELLENV})"
   bl64_fs_path_copy \
     "$BL64_VAR_DEFAULT" \
@@ -21,4 +51,5 @@ function module_zellij_setup() {
     "$BL64_VAR_DEFAULT" \
     "${home}/${SYSDEN64_PATH_SHELLENV}" \
     "${source}/${SYSDEN64_PATH_SHELLENV}"/*.env
+
 }
