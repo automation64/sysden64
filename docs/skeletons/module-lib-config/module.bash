beacf7ce621a7318240a325bcc16f0d2ead4b623
X_GIT_USER_NAME_X <X_GIT_USER_EMAIL_X>
2025-04-11 05:33:47 +0000

    doc: update

--- docs/skeletons/module-lib-config/module.bash
+++ docs/skeletons/module-lib-config/module.bash
@@ -5,18 +5,28 @@ function module_X_MODULE_ID_X_setup() {
   local module_type="$SYSDEN64_MODULE_TYPE_SHARED"
   local model='X_MODULE_ID_X'
   local source=''
-  local config='X_CONFIG_X'
-  local target="${home}/${config}"
 
   [[ -z "$(bl64_bsh_command_locate 'X_COMMAND_X')" ]] &&
     bl64_dbg_app_show_info "$SYSDEN64_TXT_NOT_DETECTED" && return 0
   bl64_msg_show_phase 'prepare X_APP_X'
 
-  source="$(module_set_model "$module_type" "$model")" ||
+  source="$(module_set_model "$module_type" "$model")" &&
+    module_X_MODULE_ID_X_setup_config "$home" "$source" "$model"
+}
+
+function module_X_MODULE_ID_X_setup_config() {
+  bl64_dbg_app_show_function "$@"
+  local home="$1"
+  local source="$2"
+  local model="$3"
+  local target_base="${home}/X_BASE_TARGET_X"
+  local config='X_CONFIG_X'
+  # local config_file='X_CONFIG_FILE_X'
+  local target="${home}/${config}"
+
+  module_config_backup "$model" "$target" ||
     return $?
 
-  module_sync_is_requested "$module_type" && return 0
-  module_config_backup "$model" "$target" || return $?
   bl64_msg_show_task "promote configuration from model (${model}/${config})"
   # shellcheck disable=SC2086
   bl64_fs_path_copy \
@@ -24,6 +34,6 @@ function module_X_MODULE_ID_X_setup() {
     "$BL64_VAR_DEFAULT" \
     "$BL64_VAR_DEFAULT" \
     "$BL64_VAR_DEFAULT" \
-    "${home}/X_BASE_TARGET_X" \
+    "$target_base" \
     "${source}/${config}"
 }
