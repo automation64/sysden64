25f9fe0bccde914af32c7408c3eba95b8243d870
X_GIT_USER_NAME_X <X_GIT_USER_EMAIL_X>
2025-04-11 05:34:06 +0000

    refact: optimize

--- src/lib/rd.bash
+++ src/lib/rd.bash
@@ -5,8 +5,6 @@ function module_rd_setup() {
   local module_type="$SYSDEN64_MODULE_TYPE_SHARED"
   local model='rd'
   local source=''
-  local config='profile'
-  local target="${home}"
 
   [[ "$BL64_OS_TYPE" != "$BL64_OS_TYPE_MACOS" ]] &&
     [[ ! -x '/Applications/Rancher Desktop.app/Contents/Resources/resources/darwin/bin/docker' ]] &&
@@ -14,23 +12,21 @@ function module_rd_setup() {
   bl64_msg_show_phase 'prepare Rancher Desktop'
 
   source="$(module_set_model "$module_type" "$model")" &&
-    module_rd_setup_env "$home" "$source" &&
-    module_rd_setup_config "$home" "$source" "$config" "$model" "$target"
+    module_setup_env "$home" "$source" &&
+    module_rd_setup_config "$home" "$source" "$model"
 }
 
 function module_rd_setup_config() {
   bl64_dbg_app_show_function "$@"
   local home="$1"
   local source="$2"
-  local config="$3"
-  local model="$4"
-  local target="$5"
-  local config_file=''
+  local model="$3"
+  local target_base="${home}/Library/Preferences"
+  local config='profile'
+  local config_file='io.rancherdesktop.profile.defaults.plist'
+  local target="${target_base}/${config_file}"
 
-  target="${target}/Library/Preferences"
-  config_file='io.rancherdesktop.profile.defaults.plist'
-
-  module_config_backup "$model" "${target}/${config_file}" ||
+  module_config_backup "$model" "${target}" ||
     return $?
 
   bl64_msg_show_task "promote configuration from model (${model}/${config})"
@@ -40,13 +36,6 @@ function module_rd_setup_config() {
     "$BL64_VAR_DEFAULT" \
     "$BL64_VAR_DEFAULT" \
     "$BL64_VAR_DEFAULT" \
-    "$target" \
+    "$target_base" \
     "${source}/${config}/${BL64_OS_TYPE}/${config_file}"
-}
-
-function module_rd_setup_env() {
-  bl64_dbg_app_show_function "$@"
-  local home="$1"
-  local source="$2"
-  module_setup_env "$home" "$source"
 }
--- src/lib/zellij.bash
+++ src/lib/zellij.bash
@@ -5,25 +5,24 @@ function module_zellij_setup() {
   local module_type="$SYSDEN64_MODULE_TYPE_SHARED"
   local model='zellij'
   local source=''
-  local config='.config/zellij'
-  local target="${home}/${config}"
 
   [[ -z "$(bl64_bsh_command_locate 'zellij')" ]] &&
     bl64_dbg_app_show_info "$SYSDEN64_TXT_NOT_DETECTED" && return 0
   bl64_msg_show_phase 'prepare Zellij'
 
   source="$(module_set_model "$module_type" "$model")" &&
-    module_zellij_setup_env "$home" "$source" &&
-    module_zellij_setup_config "$home" "$source" "$config" "$model" "$target"
+    module_setup_env "$home" "$source" &&
+    module_zellij_setup_config "$home" "$source" "$model"
 }
 
 function module_zellij_setup_config() {
   bl64_dbg_app_show_function "$@"
   local home="$1"
   local source="$2"
-  local config="$3"
-  local model="$4"
-  local target="$5"
+  local model="$3"
+  local target_base="${home}/.config"
+  local config='.config/zellij'
+  local target="${home}/${config}"
 
   module_config_backup "$model" "$target" ||
     return $?
@@ -35,14 +34,6 @@ function module_zellij_setup_config() {
     "$BL64_VAR_DEFAULT" \
     "$BL64_VAR_DEFAULT" \
     "$BL64_VAR_DEFAULT" \
-    "${home}/.config" \
+    "$target_base" \
     "${source}/${config}"
-}
-
-function module_zellij_setup_env() {
-  bl64_dbg_app_show_function "$@"
-  local home="$1"
-  local source="$2"
-  module_setup_env "$home" "$source"
-
 }
