440c3ee32a7259d9c26fdc85c6cc5ad391c5a17f
SerDigital64 (Max) <serdigital64@gmail.com>
2025-07-27 11:09:06 +0200

    real: promote 4.1.0 (#67)

---
+++ src/lib/cursor.bash
@@ -0,0 +1,16 @@
+# Version: 1.0.0
+function module_cursor_setup() {
+  bl64_dbg_app_show_function "$@"
+  local home="$1"
+  local module_type="$SYSDEN64_MODULE_TYPE_SHARED"
+  local model='cursor'
+  local source=''
+  local extra_locations='/Applications/Cursor.app/Contents/MacOS'
+
+  [[ -z "$(bl64_bsh_command_locate 'Cursor')" ]] &&
+    bl64_dbg_app_show_info "$SYSDEN64_TXT_NOT_DETECTED" && return 0
+  bl64_msg_show_phase 'prepare Cursor IDE'
+
+  source="$(module_set_model "$module_type" "$model")" &&
+    module_setup_env "$home" "$source"
+}
