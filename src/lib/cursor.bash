00511a8f2c99692c4c00fc3231652ec1d63af977
X_GIT_USER_NAME_X <X_GIT_USER_EMAIL_X>
2025-07-28 18:37:32 +0000

    fix: missing arg

--- src/lib/cursor.bash
+++ src/lib/cursor.bash
@@ -1,4 +1,4 @@
-# Version: 1.0.0
+# Version: 1.0.1
 function module_cursor_setup() {
   bl64_dbg_app_show_function "$@"
   local home="$1"
@@ -7,7 +7,7 @@ function module_cursor_setup() {
   local source=''
   local extra_locations='/Applications/Cursor.app/Contents/MacOS'
 
-  [[ -z "$(bl64_bsh_command_locate 'Cursor')" ]] &&
+  [[ -z "$(bl64_bsh_command_locate 'Cursor' "$extra_locations")" ]] &&
     bl64_dbg_app_show_info "$SYSDEN64_TXT_NOT_DETECTED" && return 0
   bl64_msg_show_phase 'prepare Cursor IDE'
 
