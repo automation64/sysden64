78e53880e81510aa36935a15394cc38a40f77d06
SerDigital64 <serdigital64@gmail.com>
2025-08-30 19:07:00 +0200

    refact: upgrade skeleton

--- src/lib/ansible.bash
+++ src/lib/ansible.bash
@@ -1,22 +1,20 @@
-# Version: 1.0.0
-# template: legacy
+# Version: 1.0.1
+# template: lib-config-1.0.0
 function module_ansible_setup() {
   bl64_dbg_app_show_function "$@"
   local home="$1"
   local module_type="$SYSDEN64_MODULE_TYPE_SHARED"
   local model='ansible'
   local source=''
-  local config='.ansible.cfg'
-  local target="${home}/${config}"
 
   [[ -z "$(bl64_bsh_command_locate 'ansible')" ]] &&
     bl64_dbg_app_show_info "$SYSDEN64_TXT_NOT_DETECTED" && return 0
   bl64_msg_show_phase 'prepare Ansible'
 
-  source="$(module_set_model "$module_type" "$model")" ||
-  return $?
+  source="$(module_set_model "$module_type" "$model")" &&
+    module_ansible_setup_config "$home" "$source" "$model"
 
-  module_sync_is_requested "$module_type" && return 0
+  module_dedicated_is_new "$module_type" && return 0
   module_config_backup "$model" "$target" || return $?
   bl64_msg_show_task "promote configuration from model (${model}/${config})"
   bl64_fs_path_copy \
@@ -25,5 +23,27 @@ function module_ansible_setup() {
     "$BL64_VAR_DEFAULT" \
     "$BL64_VAR_DEFAULT" \
     "$home" \
+    "${source}/${config}"
+}
+
+function module_ansible_setup_config() {
+  bl64_dbg_app_show_function "$@"
+  local home="$1"
+  local source="$2"
+  local model="$3"
+  local target_base="${home}"
+  local config='.ansible.cfg'
+  local target="${target_base}/${config}"
+
+  module_config_backup "$model" "$target" ||
+    return $?
+
+  bl64_msg_show_task "promote configuration from model (${model}/${config})"
+  bl64_fs_path_copy \
+    "$BL64_VAR_DEFAULT" \
+    "$BL64_VAR_DEFAULT" \
+    "$BL64_VAR_DEFAULT" \
+    "$BL64_VAR_DEFAULT" \
+    "$target_base" \
     "${source}/${config}"
 }
--- src/lib/gpg.bash
+++ src/lib/gpg.bash
@@ -1,35 +1,53 @@
 # Version: 2.0.0
-# template: legacy
+# template: lib-dedicated-1.0.0
 function module_gpg_setup() {
   bl64_dbg_app_show_function "$@"
   local home="$1"
   local module_type="$SYSDEN64_MODULE_TYPE_DEDICATED"
   local model='gpg'
   local source=''
-  local config='.gnupg'
-  local target="${home}/${config}"
-  local vault="${DEV_PATH_PROF_VAULT}/${model}"
 
   [[ -z "$(bl64_bsh_command_locate 'gpg')" ]] &&
     bl64_dbg_app_show_info "$SYSDEN64_TXT_NOT_DETECTED" && return 0
   bl64_msg_show_phase 'prepare GNUPG'
 
-  source="$(module_set_model "$module_type" "$model")" ||
+  source="$(module_set_model "$module_type" "$model")" &&
+    module_gpg_setup_config "$home" "$source" "$model"
+}
+
+function module_gpg_setup_config() {
+  bl64_dbg_app_show_function "$@"
+  local home="$1"
+  local source="$2"
+  local model="$3"
+  local target_base="${home}"
+  local config='.gnupg'
+  local target="${target_base}/${config}"
+
+  module_dedicated_is_new "$model" || return 0
+  module_config_backup "$model" "$target" ||
     return $?
 
-  bl64_lib_flag_is_enabled "$SYSDEN64_ACTION_SYNC" && return 0
-
-  module_config_backup "$model" "$target" || return $?
-  bl64_msg_show_task "setup GNUPG (${target})"
+  bl64_msg_show_task "promote configuration from model (${model}/${config})"
   if bl64_lib_flag_is_enabled "$SYSDEN64_FLAG_USER_WIDE"; then
-    if [[ ! -d "$vault" ]]; then
-      bl64_fs_dir_create "$BL64_VAR_DEFAULT" "$BL64_VAR_DEFAULT" "$BL64_VAR_DEFAULT" \
-        "$vault" ||
-        return $?
-    fi
-    bl64_fs_symlink_create "$vault" "$target" "$BL64_VAR_ON"
-  else
+    target_base="${DEV_PATH_PROF_VAULT}/${model}"
     bl64_fs_dir_create "$BL64_VAR_DEFAULT" "$BL64_VAR_DEFAULT" "$BL64_VAR_DEFAULT" \
-      "$target"
+      "$target_base" &&
+      bl64_fs_path_copy \
+        "$BL64_VAR_DEFAULT" \
+        "$BL64_VAR_DEFAULT" \
+        "$BL64_VAR_DEFAULT" \
+        "$BL64_VAR_DEFAULT" \
+        "$target_base" \
+        "${source}/${config}" &&
+      bl64_fs_symlink_create "${target_base}/${config}" "$target" "$BL64_VAR_ON"
+  else
+    bl64_fs_path_copy \
+      "$BL64_VAR_DEFAULT" \
+      "$BL64_VAR_DEFAULT" \
+      "$BL64_VAR_DEFAULT" \
+      "$BL64_VAR_DEFAULT" \
+      "$target_base" \
+      "${source}/${config}"
   fi
 }
